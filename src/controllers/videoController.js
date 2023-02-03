283ccfdc2839a1781075610a9878c15f892e0ef2
Tootin0730 <yisan1004@gmail.com>
2023-02-03 16:12:33 -0500

    6.15 Creating a Video pt.1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,4 @@
 import Video from "../models/Video"
-
 /*
 console.log("start")
 Video.find({}, (error, videos) => {
@@ -10,19 +9,21 @@ Video.find({}, (error, videos) => {
 });
 console.log("finished");
 */
-
 export const home = async (req, res) => {
     const videos = await Video.find({});
     return res.render("home", { pageTitle: "Home", videos });
 };
+
 export const watch = (req, res) => {
     const { id } = req.params;
     return res.render("watch", { pageTitle: `Watching` });
 };
+
 export const getEdit = (req, res) => {
     const { id } = req.params;
     return res.render("edit", { pageTitle: `Editing` });
 };
+
 export const postEdit = (req, res) => {
     const { id } = req.params;
     const { title } = req.body; 
@@ -34,6 +35,17 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-    const {title} = req.body;
+    const { title, description, hashtags } = req.body;
+    const video = new Video({
+        title: title,
+        description: description,
+        createdAt: Date.now(),
+        hashtags: hashtags.split(".").map((word) => `#${word}`),
+        meta: {
+            views: 0,
+            rating: 0,
+        },
+    });
+    console.log(video)
     return res.redirect("/");
 };
