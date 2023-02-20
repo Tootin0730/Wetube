268174104d8519a4ece8dfc4399f70ff25f0e462
Tootin0730 <yisan1004@gmail.com>
2023-02-20 09:34:47 -0500

    7.9 Logged in User

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -33,6 +33,7 @@ export const postJoin = async (req, res) => {
     });
   }
 };
+
 export const getLogin = (req, res) =>
   res.render("login", { pageTitle: "Login" });
 
@@ -53,8 +54,11 @@ export const postLogin = async (req, res) => {
       errorMessage: "Wrong password",
     });
   }
+  req.session.logggedin = true;
+  req.session.user = user;
   return res.redirect("/");
 };
+
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
 export const logout = (req, res) => res.send("Log out");
