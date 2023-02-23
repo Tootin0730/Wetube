3ac2ad65d28631f3d844c151c346810f8bf0ea21
Tootin0730 <yisan1004@gmail.com>
2023-02-23 10:06:35 -0500

    7.15 Environment Variables

--- src/db.js
+++ src/db.js
@@ -3,8 +3,6 @@ mongoose.set('strictQuery', true);
 mongoose.connect(process.env.DB_URL, { 
         useNewUrlParser: true, 
         useUnifiedTopology:true,
-        useFindAndModify: false,
-        useCreateIndex: true,
  });
 
 const db = mongoose.connection;
