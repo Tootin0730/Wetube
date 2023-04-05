e08420303c62bd1ed4e2b2ee19dd9f6a65f5c4b5
Tootin0730 <yisan1004@gmail.com>
2023-04-05 10:04:53 -0400

    Errors fixed
    fixed package.json names to new name

--- src/db.js
+++ src/db.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  // useFindAndModify: false,
+  // useCreateIndex: true,
 });
 
-// useFindAndModify: false,
-// useCreateIndex: true,
 const db = mongoose.connection;
 
 const handleOpen = () => console.log("✅ Connected to DB");
